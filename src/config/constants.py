92f6f68cdfbadadcef162d1320f5fe3ba2d96626
Tim Bryant <timothybryant3@gmail.com>
2024-04-03 07:56:20 -0400

    style: update compose file and change times between checks to 5mins

--- src/config/constants.py
+++ src/config/constants.py
@@ -6,7 +6,7 @@ STATUS_FILE = "status.yaml"  # f"{home_path}/.config/ppi/status.yml"
 SCHEMA_PATH = "src/schema/schema.yaml"
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 TIME_BETWEEN_INITIAL_CHECKS = 5
-TIME_BETWEEN_SCHEDULED_CHECKS = 30
+TIME_BETWEEN_SCHEDULED_CHECKS = 120
 ROOT_DIR = os.path.realpath(
     os.path.join(os.path.dirname(__file__), "../../.")
 )  # project root
